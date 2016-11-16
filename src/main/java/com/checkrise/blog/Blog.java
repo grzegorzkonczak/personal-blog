709fa92f502d7730e4808d7ec44d07377c62e780
Grzegorz Kończak <grzegorz.konczak@gmail.com>
2016-11-16 01:34:46 +0100

    Ability to edit existing entries added. Users can edit existing entries. Added some comments in controller class.

--- src/main/java/com/checkrise/blog/Blog.java
+++ src/main/java/com/checkrise/blog/Blog.java
@@ -23,28 +23,51 @@ public class Blog {
         staticFileLocation("/public");
         BlogEntryDAO dao = new InMemoryBlogEntryDAO();
 
+        // Main page, shows all blog entries
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("entries", dao.findAll());
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/detail/:slug", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("entry", dao.findBySlug(request.params("slug")));
-            return new ModelAndView(model, "detail.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/new", (request, response) -> {
-            return new ModelAndView(null, "new.hbs");
-        }, new HandlebarsTemplateEngine());
-
+        // Handles adding new blog entry
         post("/", (request, response) -> {
             String title = request.queryParams("title");
             String entry = request.queryParams("entry");
             BlogEntry blogEntry = new BlogEntry(title, entry, LocalDateTime.now());
             dao.add(blogEntry);
             response.redirect("/");
+            return null;
+        });
+
+        // shows details of chosen blog entry
+        get("/detail/:slug", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("entry", dao.findBySlug(request.params("slug")));
+            return new ModelAndView(model, "detail.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        // displays form for adding new blog entry
+        get("/new", (request, response) -> {
+            return new ModelAndView(null, "new.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        // displays form for editing blog entry
+        get("/edit/:slug", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("entry", dao.findBySlug(request.params("slug")));
+            return new ModelAndView(model, "edit.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        // handles actual editing of blog entry
+        post("/edit", (request, response) -> {
+            String title = request.queryParams("title");
+            String entry = request.queryParams("entry");
+            String slug = request.queryParams("slug");
+            BlogEntry blogEntry = dao.findBySlug(slug);
+            blogEntry.setTitle(title);
+            blogEntry.setEntry(entry);
+            response.redirect("detail/" + slug);
             return null;
         });
     }
